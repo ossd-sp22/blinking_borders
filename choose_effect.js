d97b9e84546945249eda14024aed8650b5405243
alexcasieri <ajc880@nyu.edu>
2022-02-15 12:03:26 -0500

    fixed error message

--- choose_effect.js
+++ choose_effect.js
@@ -117,7 +117,7 @@ function listenForClicks() {
 function reportExecuteScriptError(error) {
   document.querySelector("#popup-content").classList.add("hidden");
   document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute beastify content script: ${error.message}`);
+  console.error(`Failed to execute content script: ${error.message}`);
 }
 
 
